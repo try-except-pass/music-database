89b819a2bdb8ebe684991c8eb42aa6b4021aee03
try-except-pass <73493873+try-except-pass@users.noreply.github.com>
2020-10-28 02:26:41 +0000

    Add files via upload

---
+++ (1) My Artists_scrapper.py
@@ -0,0 +1,30 @@
+from mutagen.easyid3 import EasyID3 as prop
+import os
+import pickle
+
+music_folder = input("Where's your music? ")
+all_files = [os.path.join(root, name)
+             for root, dirs, files in os.walk(music_folder)
+             for name in files
+             if name.endswith((".mp3")) or name.endswith((".flac"))]
+
+full_list = {}
+full_list_low_case = {}
+
+for file in all_files:
+    properties = prop(file)
+    artist = "".join(properties.pop("artist"))
+    artist_low = "".join(e.lower() for e in artist if e.isalnum())
+    album = "".join(properties.pop("album"))
+    album_low = "".join(e.lower() for e in album if e.isalnum())
+    if artist_low not in full_list_low_case:
+        full_list[artist] = [] 
+        full_list_low_case[artist_low] = [] 
+        if album_low not in full_list_low_case[artist_low]:
+            full_list[artist].append(album)
+            full_list_low_case[artist_low].append(album_low)
+
+with open('My Artists_Albums.pickle', "ab") as file:
+    pickle.dump(full_list, file)
+
+print("Done")
